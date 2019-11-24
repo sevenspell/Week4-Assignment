5d9831e4f8c80efde831a9d7d6265a6119882ea2
Seven <sevenspell@gmail.com>
2019-11-24 22:06:30 +1100

    created new files

---
+++ questions.js
@@ -0,0 +1,30 @@
+//Pseudocoding
+
+//1. 
+
+
+
+
+
+
+
+
+
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    ///etc.
+  ];
+
+  
+
+  Init();
