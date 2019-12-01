3f693effc1042b55d8320d1bc2eb118972b698c2
Seven <sevenspell@gmail.com>
2019-12-01 14:58:25 +1100

    adding button click

--- questions.js
+++ questions.js
@@ -6,27 +6,27 @@ var listOfQuizQuestions = [
   {
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    answer: "choicelist3"
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    answer: "choicelist3"
   },
   {
     title: "What is the correct code for 'A does not equal to B'?",
     choices: ["A =/= B", "A !== B", "A === B", "A += B"],
-    answer: "A !== B"
+    answer: "choicelist2"
   },
   {
     title: "The purpose of functions is to:",
     choices: ["perform actions in the content", "style the content", "create content", "log results"],
-    answer: "perform actions in the content"
+    answer: "choicelist1"
   },
   {
     title: "What function do you use to create random results?",
     choices: ["Math.floor(x)", "Math.round(x)", "Math.ceil(x)", "Math.random()"],
-    answer: "Math.random()"
+    answer: "choicelist4"
   }
 ];
 
@@ -37,7 +37,7 @@ var currentQuestion = listOfQuizQuestions[currentQuestionIndex];
 var nextQuestionIndex = currentQuestionIndex + 1;
 var nextQuestion = listOfQuizQuestions[nextQuestionIndex];
 var hiScore = 0;
-var totalSeconds = listOfQuizQuestions.length * 15;
+var totalSeconds = (listOfQuizQuestions.length * 15) + 15;
 
 //1. instructions and start button for user to press start
   //hi-score on the top left shows original score = 0
@@ -54,7 +54,7 @@ var totalSeconds = listOfQuizQuestions.length * 15;
 
 
 //2. Once start button is clicked..
-
+  //addEventListener on button
 get("startbutton").addEventListener("click", function () {
   //hide start button
   hideStart();
@@ -68,8 +68,8 @@ get("startbutton").addEventListener("click", function () {
 
   //for (var i = 0; i < listOfQuizQuestions.length; i++) {}
 
-
 });
+
 
 
   //instructions and start button is hidden
@@ -85,6 +85,7 @@ get("startbutton").addEventListener("click", function () {
 //display question and 4 choices of answers
 
   //create function for creating html tags for questions
+
   function createHTML(){
     var questionDivElem = document.createElement("div");
     questionDivElem.setAttribute("class", "col-sm-8 offset-sm-2 col-md-8 offset-md-2 show");
@@ -101,24 +102,28 @@ get("startbutton").addEventListener("click", function () {
     var choiceListElem = document.createElement("li");
     choiceListElem.setAttribute("class", "col-sm-8 offset-sm-2 col-md-8 offset-md-2 show choiceButtons");
     choiceListElem.setAttribute("id", "choicelist1");
+    choiceListElem.setAttribute("type", "submit");
     choiceListElem.style.display = "block";
     get("quizcontainer").appendChild(choiceListElem);
 
     var choiceListElem = document.createElement("li");
     choiceListElem.setAttribute("class", "col-sm-8 offset-sm-2 col-md-8 offset-md-2 show choiceButtons");
     choiceListElem.setAttribute("id", "choicelist2");
+    choiceListElem.setAttribute("type", "submit");
     choiceListElem.style.display = "block";
     get("quizcontainer").appendChild(choiceListElem);
 
     var choiceListElem = document.createElement("li");
     choiceListElem.setAttribute("class", "col-sm-8 offset-sm-2 col-md-8 offset-md-2 show choiceButtons");
     choiceListElem.setAttribute("id", "choicelist3");
+    choiceListElem.setAttribute("type", "submit");
     choiceListElem.style.display = "block";
     get("quizcontainer").appendChild(choiceListElem);
 
     var choiceListElem = document.createElement("li");
     choiceListElem.setAttribute("class", "col-sm-8 offset-sm-2 col-md-8 offset-md-2 show choiceButtons");
     choiceListElem.setAttribute("id", "choicelist4");
+    choiceListElem.setAttribute("type", "submit");
     choiceListElem.style.display = "block";
     get("quizcontainer").appendChild(choiceListElem);
     };
@@ -126,6 +131,7 @@ get("startbutton").addEventListener("click", function () {
     
 
   // parse question and choices to html tags
+
   function renderQuestions() {
 
     var titleValue = listOfQuizQuestions[0].title;
@@ -147,6 +153,7 @@ get("startbutton").addEventListener("click", function () {
 
 
   //timer starts counting down
+  updateTimer();
   function updateTimer(){
     // start timer countdown if there is still time left
     if (totalSeconds > 0) {
@@ -173,6 +180,31 @@ get("startbutton").addEventListener("click", function () {
   
 
 //3. When choice of answer is clicked..
+    // addEventListener for choice button to return value
+
+
+
+    // function returnChoiceValue(event){
+    //   var returnChoice = event.target;
+    //   //currentQuestion = listOfQuizQuestions[currentQuestionIndex];
+
+
+    // }
+
+
+    get("choicelist2").addEventListener("click", function () {
+      
+      // function returnUserChoiceValue(event){
+      //   //var returnChoice = event.target;
+      //   var returnChoiceIDValue = returnChoice.getAttribute("id");
+      //   console.log(returnChoiceIDValue);
+      //   console.log("user chose: " + returnChoice.getAttribute("id"));
+      // };
+
+      console.log("choicelist2 is working");
+
+    });
+
   //determine if answer matches correct answer
     // if answer is correct, show text to indicate "correct"
       //hide current question/choices
